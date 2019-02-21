b9026ed5a1c0381fb9cef906934477f9ac4c1e18
Abdul Rehman <mehars.6925@gmail.com>
2019-02-22 00:53:34 +0500

    + login, register, logout routes

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -1,13 +1,39 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template, redirect, url_for
+from flask_login import login_user, login_required, logout_user
+from . import app, mail
 from ..models import db, User
 
-router = Blueprint('rotuer', __name__)
+auth = Blueprint('auth', __name__)
 
-@router.route('/')
-def auth():
-    user = User()
-    user.username = 'admin'
-    user.email = 'admin@localhost'
-    # print(user)
-    db.session.add(user)
-    return 'Hello From Auth'
+@auth.route('/login', methods=['GET', 'POST'])
+def login(): 
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        return 'POST'
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        name = request.form['name']
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        cpassword = request.form['c_password']
+        mail.send_email(
+            to_email=[{'email': 'mehars.6925@gmail.com'}, {'email': email}],
+            subject='Authentication',
+            text='Hey'
+        )
+        user = User(name, email, username, password)
+        db.session.add(user)
+        db.session.commit()
+        return username
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
