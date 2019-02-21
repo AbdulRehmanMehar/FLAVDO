4bf48c457e691b53c47a27114505a4ec30bc494f
Abdul Rehman <mehars.6925@gmail.com>
2019-02-22 00:52:59 +0500

    updated

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,14 +1,20 @@
-from .. import app
+from .. import app, mail
+from flask_login import current_user
 from flask import Blueprint, render_template
 
-index = Blueprint('controllers', __name__)
+index = Blueprint('app', __name__)
 
 @index.route('/')
 def home():
-    return render_template('index.html')
+    if(current_user.is_authenticated):
+        return current_user.username
+    else:
+        return render_template('index.html')
+    
+
 
 # IMPORT other controllers
-from .auth import router as auth
+from .auth import auth
 
 # REGISTER CONTROLLERS
 app.register_blueprint(index)
