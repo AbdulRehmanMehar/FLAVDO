c13f68c56f7252884b3df84f2892c09f1e482206
Abdul Rehman <mehars.6925@gmail.com>
2019-02-22 00:54:04 +0500

    Added more fields

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,28 @@
+from flask_login import UserMixin
 from . import db
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+class User(UserMixin, db.Model):
+    __tablename__ = 'user'
+    id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column('name', db.String(200), nullable=False)
+    email = db.Column('email',db.String(120), unique=True, nullable=False)
+    username = db.Column('username', db.String(80), unique=True, nullable=False)
+    password = db.Column('password', db.String(200), nullable=False)
+    admin = db.Column('admin', db.Boolean, nullable=False, default=False)
+    verified = db.Column('verified', db.Boolean, nullable=False, default=False)
+
+    def __init__(self, name, email, username, password, admin=False, verified=False):
+        self.name = name
+        self.email = email
+        self.username = username
+        self.password = password
+        self.admin = admin
+        self.verified = verified
+        user = User.query.order_by(User.id.desc()).first()
+        if(user == None):
+            self.id = 1
+        else:
+            self.id = user.id + 1
 
     def __repr__(self):
         return '<User %r>' % self.username
