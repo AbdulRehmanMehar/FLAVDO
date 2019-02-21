b573ca208434e20e6ae3930013ace400f6db8132
Abdul Rehman <mehars.6925@gmail.com>
2019-02-22 00:52:09 +0500

    + dynamic config, mail setup and auth system

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_sendgrid import SendGrid
 
 # INITIALIZE APPLICATION
 
@@ -12,10 +14,27 @@ app.template_folder = os.path.join(
 app.static_folder = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'public')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/videoserver'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
+config_path = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'app.config')
 
+with open(config_path) as data_file:
+    config = data_file.readlines()
+config = [x.strip() for x in config]
+
+for x in config:
+    if x != '':
+        arr = x.split('=')
+        app.config[arr[0]] = arr[1]
+
+db = SQLAlchemy(app)
+mail = SendGrid(app)
+
+login_manager = LoginManager(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 # IMPORT SUB MODULES
+from .models import *
 from .controllers import *
