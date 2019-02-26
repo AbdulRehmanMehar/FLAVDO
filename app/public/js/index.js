423492d31549d9f295a3791f43865d88c68c74a9
Abdul Rehman <mehars.6925@gmail.com>
2019-02-26 23:36:08 +0500

    adds active class to current navigation link

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,1 +1,7 @@
-console.log(window.location.href);
+$(document).ready(() => {
+    let href = window.location.href.split('/').pop();
+    $('.nav-link').each((i, el) => {
+        let hr = $(el).attr('href').split('/').pop();
+        if(href === hr) $(el).parent().addClass('active');
+    });
+});
