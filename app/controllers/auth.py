c696d3c9666a315965fd173d7535e2575a766fed
Abdul Rehman <mehars.6925@gmail.com>
2019-02-26 23:34:08 +0500

    implemented wtforms

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -2,35 +2,31 @@ from flask import Blueprint, request, render_template, redirect, url_for
 from flask_login import login_user, login_required, logout_user
 from . import app, mail
 from ..models import db, User
+from ..forms import LoginForm, RegisterationForm
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login(): 
-    if request.method == 'GET':
-        return render_template('login.html')
-    else:
-        return 'POST'
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return form.username.data
+    return render_template('login.html', form=form)
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'GET':
-        return render_template('register.html')
-    else:
-        name = request.form['name']
-        email = request.form['email']
-        username = request.form['username']
-        password = request.form['password']
-        cpassword = request.form['c_password']
-        mail.send_email(
-            to_email=[{'email': 'mehars.6925@gmail.com'}, {'email': email}],
-            subject='Authentication',
-            text='Hey'
-        )
-        user = User(name, email, username, password)
+    form = RegisterationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.name.data, form.email.data, form.username.data, form.password.data)
+        # mail.send_email(
+        #     to_email=[{'email': email}],
+        #     subject='Authentication',
+        #     html=''
+        # )
         db.session.add(user)
         db.session.commit()
-        return username
+        return user.name
+    return render_template('register.html', form=form)
 
 @auth.route('/logout')
 @login_required
