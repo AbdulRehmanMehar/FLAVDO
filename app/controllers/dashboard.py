8ad578808d755d661ec93f65c1b223e7e4531173
Abdul Rehman <mehars.6925@gmail.com>
2019-03-05 22:44:27 +0500

    + dashboard route and view

--- app/controllers/dashboard.py
+++ app/controllers/dashboard.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request, abort, flash
+from flask import Blueprint, render_template, redirect, url_for, request, abort, flash, Markup
 from flask_login import current_user
 
 dashboard = Blueprint('dashboard', __name__)
@@ -8,8 +8,12 @@ def restrict():
     if(not current_user.is_authenticated):
         return abort(401)
     if(not current_user.verified):
-        flash('Please verify your email.', 'info')
+        flash('Please verify your email. '+ Markup('<a class="alert-link" href="'+ url_for('auth.send_token') +'">Resend Verification Token?</a>'), 'info')
 
 @dashboard.route('/')
 def home():
-    return 'Hey'
+    return render_template('dashboard.html')
+
+@dashboard.route('/edit-profile')
+def edit_profile():
+    return render_template('dashboard.html')
