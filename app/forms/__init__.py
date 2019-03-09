22a5bc80623ec913154a7e5ab0d05cbf13a407cf
Abdul Rehman <mehars.6925@gmail.com>
2019-03-10 01:24:56 +0500

    added more forms

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,6 @@ from .. import app
 from .login import LoginForm
 from .registeration import RegisterationForm
 from .photo import PhotoUploadForm
+from .update_profile import EditProfileForm
+from .upload_video import VideoUploadForm
+from .video import VideoForm
