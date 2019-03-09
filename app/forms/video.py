bb0e54309018039f78f0f6159785b1d3a6c97ff3
Abdul Rehman <mehars.6925@gmail.com>
2019-03-10 01:27:18 +0500

    video fields validation form

---
+++ app/forms/video.py
@@ -0,0 +1,11 @@
+from wtforms import Form, StringField, TextAreaField, validators, ValidationError
+from ..models import User
+
+class VideoForm(Form):
+    title = StringField('Title for video', [
+        validators.DataRequired()
+    ], description='Lorem ipsum dolor sit amet')
+
+    description = TextAreaField('Description for video', [
+        validators.length(min=50, max=500, message='Minimum 50 and Maximum 500 characters are allowed')
+    ], description='Optional, but minimum 50 characters long if provided')
