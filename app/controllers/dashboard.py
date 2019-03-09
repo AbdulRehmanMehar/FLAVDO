2f45da6a88c32db0435cf8595a0a11b98f9d99bd
Abdul Rehman <mehars.6925@gmail.com>
2019-03-10 01:35:41 +0500

    video upload and remove routes

--- app/controllers/dashboard.py
+++ app/controllers/dashboard.py
@@ -1,5 +1,8 @@
+import os
 from flask import Blueprint, render_template, redirect, url_for, request, abort, flash, Markup
 from flask_login import current_user
+from ..models import Video, app, db
+from ..forms import VideoUploadForm, VideoForm
 
 dashboard = Blueprint('dashboard', __name__)
 
@@ -8,7 +11,7 @@ def restrict():
     if(not current_user.is_authenticated):
         return abort(401)
     if(not current_user.verified):
-        flash('Please verify your email. '+ Markup('<a class="alert-link" href="'+ url_for('auth.send_token') +'">Resend Verification Token?</a>'), 'info')
+        flash('Please verify your email. '+ Markup('<a class="alert-link" href="'+ url_for('auth.send_token', uname=current_user.username) +'">Resend Verification Token?</a>'), 'info')
     if(current_user.photo == None):
         flash(Markup('<a href="'+ url_for('auth.upload_photo') +'">Add a photo?</a>') + ' So others may recognize your channel.', 'info')
 
@@ -16,6 +19,31 @@ def restrict():
 def home():
     return render_template('dashboard.html')
 
-@dashboard.route('/edit-profile')
-def edit_profile():
-    return render_template('dashboard.html')
+@dashboard.route('/upload-video', methods=['GET', 'POST'])
+def upload_video():
+    form = VideoForm(request.form)
+    vform = VideoUploadForm(request.files)
+    if request.method == 'POST' and form.validate() and vform.validate():
+        vfile = request.files[vform.video.name]
+        vfname = form.title.data.replace(' ', '_') + '_by_' + current_user.username + '.' + vform.ext
+        print(vfname)
+        video = Video(form.title.data, form.description.data, vfname, current_user.get_id())
+        vfile.save(os.path.join(app.config['UPLOADS_FOLDER'] + '/videos', vfname))
+        db.session.add(video)
+        db.session.commit()
+        flash('Video has been uploaded.', 'success')
+        redir = request.args.get('next') or request.referrer or url_for('auth.login')
+        return redirect(redir)
+    return render_template('upload-video.html', form=form, vform=vform)
+
+@dashboard.route('/remove-video/<id>')
+def remove_video(id):
+    video = Video.query.filter(Video.id == id and Video.owner_id == current_user.get_id()).first()
+    if video != None:
+        db.session.delete(video)
+        db.session.commit()
+        flash('Video has been removed successfully.', 'success')
+    else: 
+        flash('Sorry, Something went wrong.', 'danger')
+    redir = request.args.get('next') or request.referrer or url_for('auth.login')
+    return redirect(redir)
