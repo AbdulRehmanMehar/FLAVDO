b98b1d6b0e5847699b9a27188df903910d4737b1
Abdul Rehman <mehars.6925@gmail.com>
2019-03-10 01:25:51 +0500

    added html markup

--- app/forms/login.py
+++ app/forms/login.py
@@ -1,4 +1,5 @@
 from wtforms import Form, StringField, PasswordField, validators, ValidationError
+from flask import Markup, url_for
 from ..models import User
 
 class LoginForm(Form):
@@ -9,15 +10,15 @@ class LoginForm(Form):
     def validate_username(self, field):
         self.user = User.query.filter((User.email == field.data) | (User.username == field.data)).first()
         if self.user == None:
-            raise ValidationError('Username or Email was not found in record.')
+            raise ValidationError('Username or Email was not found in record.'  + Markup('<a href='+ url_for('auth.register') +' style="float: right">Register?</a>'))
     
     password = PasswordField('Password', [
         validators.DataRequired()
     ], description='**********')
 
     def validate_password(self, field):
-        if self.user == None or not self.user.compare(field.data):
-            raise ValidationError('Incorrect Credentials were provided.')
+        if (self.user == None or not self.user.compare(field.data)) and not len(self.username.errors) > 0:
+            raise ValidationError('Incorrect Credentials were provided. ' + Markup('<a href='+ url_for('auth.send_token', uname=self.username.data) +' style="float: right">Reset Password?</a>'))
 
     def get_user(self):
         return self.user
